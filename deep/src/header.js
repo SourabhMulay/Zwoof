fb9ac4f35d19fbed5e0e76daff09476b54ba51bc
Sourabh <Mulaysrbh4@gmail.com>
2025-03-11 09:02:09 +0530

    Deploy: initial changes

---
+++ deep/src/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+export default function header(){
+    return (
+
+        <h1 class="text-3xl font-extralight">header</h1>
+    );
+}
+
