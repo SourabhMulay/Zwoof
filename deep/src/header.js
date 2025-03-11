2ed250a4258e96deac73342788a69d2cf4e6c217
Sourabh <Mulaysrbh4@gmail.com>
2025-03-11 11:53:02 +0530

    Changes

--- deep/src/header.js
+++ deep/src/header.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 export default function header(){
     return (
